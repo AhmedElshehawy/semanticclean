2a7f398e238f85c1e641fc81901c804f2fbcf821
AhmedElshehawy <ahmedelshehawy64@gmail.com>
2023-06-11 20:47:45 +0300

    fix bug in report method

--- label.py
+++ label.py
@@ -1,10 +1,24 @@
+"""Simple implementation of the paper: Confident Learning: Estimating Uncertainty in Dataset Labels."""
+
+
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
 from typing import List, Dict
 
+
 class LabelIssues:
-    def __init__(self, labels, pred_probs):
+    """Class that finds issues in data labels."""
+
+    def __init__(self, labels: np.ndarray, pred_probs: np.ndarray):
+        """Initialize the class with labels and prediction probabilities.
+
+        Args:
+            labels (np.ndarray): the noisy (given) labels of shape (n,),
+            n is the number of examples.
+            pred_probs (np.ndarray): prediction probabilities of shape (n, m), 
+            n is the number of examples, and m is the number of classes.
+        """
         self.labels = np.array(labels)
         self.pred_probs = pred_probs
         self.epsilon = 1e-6
@@ -14,9 +28,8 @@ class LabelIssues:
         self.guessed_labels = []
         self.score_list = []
         self.confident_joint_mat = None
-        
-        
-    def _get_class_thresholds(self, labels, pred_probs, epsilon, verbose):
+
+    def _get_class_thresholds(self, labels: np.ndarray, pred_probs:np.ndarray, epsilon: float, verbose: bool):
         if verbose:
             print('--> Calculating class thresholds...\n')
         # initialize empty 2D matrix with shape (num_examples, num_classes)
@@ -29,10 +42,10 @@ class LabelIssues:
 
         # now, we get class_thresholds with shape (num_classes, )
         # add epsilon to avoid dividing by zero
-        class_thresholds = np.sum(self.pred_probs * y_eq_k, axis=0) / (nk + epsilon)
+        class_thresholds = np.sum(
+            self.pred_probs * y_eq_k, axis=0) / (nk + epsilon)
 
         return class_thresholds
-
 
     def _get_confident_joint_mat(self, labels, pred_probs, class_thresholds, rank_by, verbose):
         if verbose:
@@ -62,11 +75,11 @@ class LabelIssues:
                     # so this class(j) may be the true label for this example(i)
                     guessed_true_label = j
 
-            if cnt > 1: # if cnt > 1, this means we need to break the tie
+            if cnt > 1:  # if cnt > 1, this means we need to break the tie
                 guessed_true_label = np.argmax(pred_probs[i])
-            
+
             # now, we increase the corresponding cell in the confident joint matrix
-                
+
             if cnt > 0:
                 conf_mat[noisy_label][guessed_true_label] += 1
 
@@ -78,28 +91,30 @@ class LabelIssues:
                     # append the index of this example to the the list of indices that may have label issues
                     idx_list.append(i)
                     guessed_labels_list.append(guessed_true_label)
-                    rank_score_list.append(self._rank_score(pred_probs[i][noisy_label], max_prob, by=rank_by))
+                    rank_score_list.append(self._rank_score(
+                        pred_probs[i][noisy_label], max_prob, by=rank_by))
         return conf_mat, np.array(idx_list), np.array(guessed_labels_list), np.array(rank_score_list)
 
-
     def detect_issues(self, rank_by='normalized_margin', verbose=False):
-        
-        class_thresholds = self._get_class_thresholds(self.labels, self.pred_probs, self.epsilon, verbose)
-        conf_mat, idx_arr, guessed_labels_arr, rank_score_arr = self._get_confident_joint_mat(self.labels, self.pred_probs, class_thresholds, rank_by, verbose)
-        
+
+        class_thresholds = self._get_class_thresholds(
+            self.labels, self.pred_probs, self.epsilon, verbose)
+        conf_mat, idx_arr, guessed_labels_arr, rank_score_arr = self._get_confident_joint_mat(
+            self.labels, self.pred_probs, class_thresholds, rank_by, verbose)
+
         if len(idx_arr) > 0:
             sort_indices = np.argsort(rank_score_arr)
             self.idx_issues = idx_arr[sort_indices]
             self.guessed_labels = guessed_labels_arr[sort_indices]
             self.score_list = np.sort(rank_score_arr)
-            
+
         self.confident_joint_mat = conf_mat
-    
+
     def _rank_score(self, noisy_label_prob, max_prob, by='normalized_margin'):
-        if by=='normalized_margin':
+        if by == 'normalized_margin':
             return noisy_label_prob - max_prob
-    
-    def prune(self, *args, frac: float=0.4, n: int=0, axis: int=0) -> List[np.ndarray]:
+
+    def prune(self, *args, frac: float = 0.4, n: int = 0, axis: int = 0) -> List[np.ndarray]:
         """_summary_
 
         Args:
@@ -114,7 +129,7 @@ class LabelIssues:
             num_to_remove = n
         else:
             num_to_remove = int(frac * len(self.idx_issues))
-            
+
         to_remove = self.idx_issues[:num_to_remove]
         new_data = []
         if len(args) > 0:
@@ -123,35 +138,36 @@ class LabelIssues:
                 new_data.append(arr)
         new_data.append(np.delete(self.labels, to_remove, axis=axis))
         return new_data
-        
-    def suggest(self, frac: float=0.4, n: int=0):
+
+    def suggest(self, frac: float = 0.4, n: int = 0):
         # provide a new version of data with suggested labels
         if n > 0:
             num_to_return = n
         num_to_return = int(frac * len(self.guessed_labels))
         return self.guessed_labels[:num_to_return]
-    
-    def report(self, include_cols: Dict[str, np.ndarray]={}):
-        data_cols = {'example': self.idx_issues, 'given_label': self.labels[self.idx_issues], \
-             'score': self.score_list, 'guessed_label': self.guessed_labels}
-        
+
+    def report(self, include_cols: Dict[str, np.ndarray] = {}):
+        data_cols = {'example': self.idx_issues, 'given_label': self.labels[self.idx_issues],
+                     'score': self.score_list, 'guessed_label': self.guessed_labels}
+
         if len(include_cols) > 0:
-            include_cols = {'_'+k: v[:len(self)] for k, v in include_cols.items()}
-            data_cols = {**include_cols,**data_cols}
-        
+            include_cols = {'_'+k: v[self.idx_issues] for k, v in include_cols.items()}
+            data_cols = {**include_cols, **data_cols}
+
         df = pd.DataFrame(data_cols)
         return df
-    
+
     def summary(self):
         print('-'*79)
         for i in range(self.num_classes):
             for j in range(self.num_classes):
-                if i==j:
+                if i == j:
                     continue
                 num_wrong_examples = int(self.confident_joint_mat[i][j])
                 if num_wrong_examples > 0:
                     text = f'--> {num_wrong_examples} example(s) is(are) labeled {i} but should be labeled {j} <--'
                     print(text)
         print('-'*79)
+
     def __len__(self):
         return len(self.idx_issues)
